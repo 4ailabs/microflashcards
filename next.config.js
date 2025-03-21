91645ae1f5c7a41996d24ec481daa7d5adcf5d65
4ailabs <tu-email@gmail.com>
2025-03-21 15:33:53 -0600

    Solucionar problemas de despliegue en Railway

--- next.config.js
+++ next.config.js
@@ -1,9 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false, // Cambiado a false para evitar renders dobles
   swcMinify: true,
   // Configura Railway para usar exportación estática
   output: 'standalone',
+  // Solucionar problemas de CORS
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
+  // Solucionar problemas con CSS
+  compiler: {
+    // Removes the reactStrictMode warning when using styled-jsx
+    styledComponents: true,
+  },
 }
 
 module.exports = nextConfig
