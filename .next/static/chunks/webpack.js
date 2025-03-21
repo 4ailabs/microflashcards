6c808da89b455c4761adbde908596a212123153f
4ailabs <tu-email@gmail.com>
2025-03-21 15:59:15 -0600

    Ajustar configuración para Next.js 12

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b0d9990991a2b576"; }
+/******/ 		__webpack_require__.h = function() { return "f3e9f6f444330763"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1222,6 +1222,11 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	!function() {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: false, // Desactivado para evitar problemas de minificación
-  output: 'export', // Cambiado a export para generar archivos estáticos
+  // Este tipo de salida es mejor para despliegues de Vercel en Next.js 12
+  // Para exportación estática, se usará el comando 'next export'
   trailingSlash: true, // Añadir trailing slash para mejor compatibilidad
   images: {
     unoptimized: true // Necesario para exportación estática
